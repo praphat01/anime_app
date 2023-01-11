9eafc56758e46764912348bbd7d2d1cee2ad31c9
praphat <praphat.phanomrat@hotmail.com>
2023-01-11 09:12:05 +0700

    update 11jan

--- lib/pages/mainpage.dart
+++ lib/pages/mainpage.dart
@@ -28,7 +28,7 @@ class _MainPageState extends State<MainPage> {
   var isDeviceConnected = false;
   List<Widget> _widgetList = [
     HomePage(),
-    bookshelf(),
+    bookshelfOffline(),
     bookCategory(),
     bookPublisher(),
   ];
